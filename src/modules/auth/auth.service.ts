8c83bf22f67911522d81ea640c6065be7c94a748
alireza_bisout <alirezabisout1145@gmail.com>
2026-01-08 13:06:35 +0330

    feat: enhance AuthService with user validation and improve AuthDto type safety
    - virgol  => 7

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthType } from './enums/type.enums';
+import { AuthMethod } from './enums/method.enums';
+import { isEmail, isMobilePhone } from 'class-validator';
 
 @Injectable()
 export class AuthService {
     userExistence(authDto: AuthDto) {
-        return authDto
+        const { method, type, username } = authDto
+
+        switch (type) {
+            case AuthType.Register:
+                return this.register(method, username)
+
+            case AuthType.Login:
+
+                return this.login(method, username)
+
+
+            default:
+                throw new UnauthorizedException()
+        }
+    }
+
+    login(method: AuthMethod, username: string) {
+        return this.usernameValidator(method, username)
+
+    }
+
+    register(method: AuthMethod, username: string) {
+        return this.usernameValidator(method, username)
+    }
+
+    usernameValidator(method: AuthMethod, username: string) {
+        switch (method) {
+            case AuthMethod.Email:
+
+                if (isEmail(username)) return username
+                throw new BadRequestException("فرمت ایمیل اشتباه است!!")
+
+            case AuthMethod.Phone:
+                if (isMobilePhone(username, "fa-IR")) return username
+                throw new BadRequestException("فرمت موبایل اشتباه است!!")
+
+            case AuthMethod.Username:
+                return username
+
+            default:
+                throw new UnauthorizedException("اطلاعات فرستاده شده صحیح نیست")
+        }
     }
 }
